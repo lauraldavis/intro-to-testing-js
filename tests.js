126d677fddefe27a83a99d7caaf4c1e7bf4d793f
Laura Davis <laura.l.davis.210@gmail.com>
2020-04-23 15:21:05 -0500

    add test for Exercise #5 to the sayHello() test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -47,4 +47,7 @@ describe('sayHello', function() {
     it('sayHello("Alex") should return "Hello, Alex!".', function() {
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
+    it('sayHello("Pat") should return "Hello, Pat!".', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
 });
