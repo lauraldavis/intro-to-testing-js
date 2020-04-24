9513ccaa0eba6c4846b83702954bdb2a576d9f23
Laura Davis <laura.l.davis.210@gmail.com>
2020-04-23 20:03:49 -0500

    more unit tests for Exercise 12 isVowel() function and Exercise 13 add() function

--- code.js
+++ code.js
@@ -5,17 +5,31 @@ function helloWorld() {
 
 // sayHello function
 function sayHello(input) {
-    return ( typeof input === "string" && isNaN(parseFloat(input)) && input !== "" ) ? "Hello, " + input + "!" : "Hello, World!";
+    return (typeof input === "string" && isNaN(parseFloat(input)) && input !== "") ? "Hello, " + input + "!" : "Hello, World!";
 }
 
+// isFive function
 function isFive(input) {
     return (input == 5 && (typeof input === "number" || typeof input === "string"));
 }
 
+// isEven function
 function isEven(input) {
-    return ( (typeof input === "string" || typeof input === "number") && parseFloat(input) % 2 === 0 ) ? true : false;
+    return ((typeof input === "string" || typeof input === "number") && parseFloat(input) % 2 === 0);
 }
 
+// isVowel function
 function isVowel(input) {
-    return (input !== "y") ? true: false;
+    var vowels = ['a', 'e', 'i', 'o', 'u'];
+    return (typeof input === "string" && (vowels.indexOf(input.toLowerCase()) > -1));
+}
+
+// add function
+function add(input1, input2) {
+    return (
+        (typeof input1 === "number" || typeof input1 === "string") &&
+        (typeof input2 === "number" || typeof input2 === "string")
+    ) ?
+        parseFloat(input1) + parseFloat(input2) :
+        "NaN";
 }
--- tests.js
+++ tests.js
@@ -53,11 +53,8 @@ describe('sayHello', function() {
     it('sayHello() should return "Hello, World!" when passed no input.', function() {
         expect(sayHello()).toBe("Hello, World!");
     });
-    it('sayHello(true) should return "Hello, World!" when passed boolean inputs.', function() {
+    it('sayHello(true) should return "Hello, World!" when passed a boolean input.', function() {
         expect(sayHello(true)).toBe("Hello, World!");
-    });
-    it('sayHello(false) should return "Hello, World!" when passed boolean inputs.', function() {
-        expect(sayHello(false)).toBe("Hello, World!");
     });
     it('sayHello(null) should return "Hello, World!" when passed null as input.', function() {
         expect(sayHello(null)).toBe("Hello, World!");
@@ -161,6 +158,12 @@ describe('isEven', function() {
     it('should return false when passed an object as input', function() {
         expect(isEven({6:6})).toBe(false);
     });
+    it('should return true when passed 0 as input', function() {
+        expect(isEven(0)).toBe(true);
+    });
+    it('should return false when passed 4.8 as input', function() {
+        expect(isEven(4.8)).toBe(false);
+    });
 });
 
 describe('isVowel', function() {
@@ -178,5 +181,83 @@ describe('isVowel', function() {
     });
     it('should return false when passed "y" as input', function() {
         expect(isVowel("y")).toBe(false);
+    });
+    it('should return false when passed 4 as input', function() {
+        expect(isVowel(4)).toBe(false);
+    });
+    it('should return false when passed a boolean as input', function() {
+        expect(isVowel(true)).toBe(false);
+    });
+    it('should return false when passed a boolean as input', function() {
+        expect(isVowel(false)).toBe(false);
+    });
+    it('should return false when passed "banana" as input', function() {
+        expect(isVowel("banana")).toBe(false);
+    });
+    it('should return false when passed no input', function() {
+        expect(isVowel()).toBe(false);
+    });
+    it('should return false when passed "" as input', function() {
+        expect(isVowel("")).toBe(false);
+    });
+    it('should return false when passed null as input', function() {
+        expect(isVowel(null)).toBe(false);
+    });
+    it('should return false when passed an array as input', function() {
+        expect(isVowel(["a","e","i","o","u"])).toBe(false);
+    });
+    it('should return false when passed an object as input', function() {
+        expect(isVowel({vowel: "a"})).toBe(false);
+    });
+    it('should return true when passed "E" as input', function() {
+        expect(isVowel("E")).toBe(true);
+    });
+    it('should return false when passed "b" as input', function() {
+        expect(isVowel("b")).toBe(false);
+    });
+});
+
+describe('add', function() {
+    it('should be a defined function', function() {
+        expect(typeof add).toBe('function');
+    });
+    it('should return 5 when passed (2, 3) as input', function() {
+        expect(add(2, 3)).toBe(5);
+    });
+    it('should return -12 when passed (-3, -9) as input', function() {
+        expect(add(-3, -9)).toBe(-12);
+    });
+    it('should return 11 when passed ("5", 6) as input', function() {
+        expect(add("5", 6)).toBe(11);
+    });
+    it('should return 6 when passed ("-4", 10) as input', function() {
+        expect(add("-4", 10)).toBe(6);
+    });
+    it('should return NaN when passed ("banana", "split") as input', function() {
+        expect(isNaN(add("banana", "split"))).toBe(true);
+    });
+    it('should return NaN when passed (2, "apples") as input', function() {
+        expect(isNaN(add(2, "apples"))).toBe(true);
+    });
+    it('should return NaN when passed no input', function() {
+        expect(isNaN(add())).toBe(true);
+    });
+    it('should return 5 when passed (1.5, 3.5) as input', function() {
+        expect(add(1.5, 3.5)).toBe(5);
+    });
+    it('should return NaN when passed a boolean as an input', function() {
+        expect(isNaN(add(true, 2))).toBe(true);
+    });
+    it('should return NaN when passed a null as an input', function() {
+        expect(isNaN(add(null, 2))).toBe(true);
+    });
+    it('should return NaN when passed "" as an input', function() {
+        expect(isNaN(add("", 2))).toBe(true);
+    });
+    it('should return NaN when passed an array as an input', function() {
+        expect(isNaN(add(2, [2,1]))).toBe(true);
+    });
+    it('should return NaN when passed an object as an input', function() {
+        expect(isNaN(add(2, {id:2}))).toBe(true);
     });
 });
